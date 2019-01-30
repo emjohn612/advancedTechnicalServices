fa0cdb7e030cdd8566dcc7e0aa5e97472f84f64c
Eric Johnston <emjohnston@liberty.edu>
2019-01-30 16:16:14 -0500

    quick edits for moving things to php. not finish build

---
+++ includes/header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+
+<html lang="en">
+  <head>
+    <title><?php echo TITLE ?></title>
+    <meta charset="UTF-8">
+    <meta name="description" content="Website for Advanced Technical Services Inc.">
+    <meta name="keywords" content="HTML,CSS,JavaScript">
+    <meta name="author" content="Eric Johnston">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/styles.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </head>
+  <body>
+
+    <div class="jumbotron text-center">
+      <h1>Advanced Technical Services</h1>
+      <p>Commercial and Residential HVAC</p>
+      <form class="form-inline">
+        <div class="input-group">
+          <input type="email" class="form-control" size="50" placeholder="Email Address" required>
+          <div class="input-group-btn">
+            <button type="button" class="btn btn-danger">Request a Quote</button>
+          </div>
+        </div>
+      </form>
+    </div>
+      <nav class="navbar navbar-default navbar-fixed-top">
+        
+      </nav>
--- index.php
+++ index.php
@@ -1,56 +1,7 @@
-<!DOCTYPE html>
-
-<html lang="en">
-  <head>
-    <title>Advanced Technical Services</title>
-    <meta charset="UTF-8">
-    <meta name="description" content="Website for Advanced Technical Services Inc.">
-    <meta name="keywords" content="HTML,CSS,JavaScript">
-    <meta name="author" content="Eric Johnston">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/styles.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-  </head>
-  <body>
-
-    <div class="jumbotron text-center">
-      <h1>Advanced Technical Services</h1>
-      <p>Commercial and Residential HVAC</p>
-      <form class="form-inline">
-        <div class="input-group">
-          <input type="email" class="form-control" size="50" placeholder="Email Address" required>
-          <div class="input-group-btn">
-            <button type="button" class="btn btn-danger">Request a Quote</button>
-          </div>
-        </div>
-      </form>
-    </div>
-      <nav class="navbar navbar-default navbar-fixed-top">
-        <div class="container">
-          <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">ATS</a>
-          </div>
-          <div class="collapse navbar-collapse" id="myNavbar">
-            <ul class="nav navbar-nav navbar-right">
-              <li><a href="#about">ABOUT</a></li>
-              <li><a href="#services">SERVICES</a></li>
-              <li><a href="#gallery">GALLERY</a></li>
-              <li><a href="#pricing">PRICING</a></li>
-              <li><a href="#contact">CONTACT</a></li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+<?php 
+  define("TITLE", "Home | Advanced Technical Services")
+  include('includes/header.php')
+?>
       <main>
 
         <div id="about" class="container-fluid">
