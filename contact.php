c5b17fa8ce87c1b1ea166457e008ba91e1159e7d
Eric Johnston <emjohnston@liberty.edu>
2019-02-21 10:20:47 -0500

    add header section for page

--- contact.php
+++ contact.php
@@ -1,6 +1,13 @@
 <?php include("includes/header.php"); ?>
-<div id="contact" class="">
-            <form>
+<div id="contact" class="container">
+<div class="container">
+    <div class="row">
+            <div class="col-12 col-sm-12">
+              <h3>Header</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            </div>
+</div>
+            <form class="container-fluid">
               <div class="form-group">
                 <div class="form-row">
                   <div class="col-md-6 mb-3">
