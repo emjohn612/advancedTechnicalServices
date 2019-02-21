35202b7dbbe94cba835067610016860946722176
Eric Johnston <emjohnston@liberty.edu>
2019-02-21 10:23:04 -0500

    add nav links by using array

--- includes/nav.php
+++ includes/nav.php
@@ -1,7 +1,7 @@
-<ul class="navbar-nav">
-        <li class="nav-item"><a class="nav-link" href="#about">ABOUT</a></li>
-        <li class="nav-item"><a class="nav-link" href="#services">SERVICES</a></li>
-        <li class="nav-item"><a class="nav-link" href="#gallery">GALLERY</a></li>
-        <li class="nav-item"><a class="nav-link" href="#pricing">PRICING</a></li>
-        <li class="nav-item"><a class="nav-link" href="#contact">CONTACT</a></li>
-    </ul>
+<ul class="collapse navbar-collapse navbar-nav" id="navbarTogglerDemo01">
+    <?php
+        foreach ($navItems as $item) {
+            echo "<li class=\"nav-item\"><a class=\"nav-link\" href=\"$item[slug]\">$item[title]</a></li>";
+        }
+    ?>
+</ul>
