7d567d87b09854a3bfa89944d0ecf88b0158fac8
Eric Johnston <emjohnston@liberty.edu>
2019-02-21 10:22:01 -0500

    add array of nav links

---
+++ includes/arrays.php
@@ -0,0 +1,28 @@
+<?php
+
+    $navItems = array(
+
+        array(
+            "slug"	=> "services.php",
+            "title"	=> "Services"
+        ),
+        
+        array(
+            "slug"	=> "gallery.php",
+            "title"	=> "Gallery"
+        ),
+        
+        array(
+            "slug"	=> "pricing.php",
+            "title"	=> "Pricing"
+        ),
+
+        array(
+            "slug"	=> "contact.php",
+            "title"	=> "Contact"
+        ),
+
+    );
+
+
+?>
