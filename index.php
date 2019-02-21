94bd2340c3285e2e09465337bb532eaa5d700f32
Eric Johnston <emjohnston@liberty.edu>
2019-02-21 10:21:31 -0500

    extend margins

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
   define("TITLE", "Home | Advanced Technical Services");
   include('includes/header.php');
 ?>
-<div id="about" class="container-fluid">
+<div id="about" class="container">
   <div class="row">
     <div class="col-sm-8">
       <h2>About</h2>
@@ -48,18 +48,16 @@
         </div>
       </div>
     </div>
-    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
       <span class="sr-only">Previous</span>
     </a>
-    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
       <span class="carousel-control-next-icon" aria-hidden="true"></span>
       <span class="sr-only">Next</span>
     </a>
   </div>
 </div>
-
-
 
 <?php 
   include('includes/footer.php');
