002b4607498d3a8455d9a167c562e8357e57dfc3
Eric Johnston <emjohnston@liberty.edu>
2019-03-05 14:19:48 -0500

    fix spacing

--- includes/header.php
+++ includes/header.php
@@ -1,45 +1,49 @@
 <?php
-    include('includes/arrays.php');
+include('includes/arrays.php');
 ?>
 
 <!DOCTYPE html>
 
 <html lang="en">
-  <head>
-    <title><?php echo TITLE ?></title>
-    <meta charset="UTF-8">
-    <meta name="description" content="Website for Advanced Technical Services Inc.">
-    <meta name="keywords" content="HTML,CSS,JavaScript">
-    <meta name="author" content="Eric Johnston">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/styles.css">
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
-    </head>
-  <body>
-  <div class="container-fluid" style ="padding: 0;">
-      <nav class="navbar navbar-expand-sm navbar-dark bg-dark justify-content-between">
-      <a class="navbar-brand" href="index.php">ATS</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <?php include('includes/nav.php'); ?>
-      </nav>
-    </div>
-<header class="container-fluid">
-    <div class="jumbotron text-center vertical-center">
-      <h1>Advanced Technical Services</h1>
-      <p>Commercial and Residential HVAC</p>
-      <form class="form-inline">
-        <div class="input-group">
-          <input type="email" class="form-control" size="50" placeholder="Email Address" required>
-          <div class="input-group-btn">
-            <button type="button" class="btn btn-danger">Request a Quote</button>
-          </div>
-        </div>
-      </form>
-    </div>
-</header>
+
+<head>
+	<title>
+		<?php echo TITLE ?>
+	</title>
+	<meta charset="UTF-8">
+	<meta name="description" content="Website for Advanced Technical Services Inc.">
+	<meta name="keywords" content="HTML,CSS,JavaScript">
+	<meta name="author" content="Eric Johnston">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+	<link rel="stylesheet" href="css/styles.css">
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+</head>
+
+<body>
+	<div class="container-fluid" style="padding: 0;">
+		<nav class="navbar navbar-expand-sm navbar-dark bg-dark justify-content-between">
+			<a class="navbar-brand" href="index.php">ATS</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<?php include('includes/nav.php'); ?>
+		</nav>
+	</div>
+	<header class="container-fluid">
+		<div class="jumbotron text-center vertical-center">
+			<h1>Advanced Technical Services</h1>
+			<p>Commercial and Residential HVAC</p>
+			<form class="form-inline">
+				<div class="input-group">
+					<input type="email" class="form-control" size="50" placeholder="Email Address" required>
+					<div class="input-group-btn">
+						<button type="button" class="btn btn-danger">Request a Quote</button>
+					</div>
+				</div>
+			</form>
+		</div>
+	</header> 
